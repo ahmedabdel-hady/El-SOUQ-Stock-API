28e6b857a10d455bae947b688d3556128a0b03d8
Ahmed Abdelhady <ahmed.abdelhady@ejust.edu.eg>
2023-05-07 08:45:41 +0300

    Update StockPricePredictor.py

--- StockPricePredictor.py
+++ StockPricePredictor.py
@@ -13,11 +13,10 @@ import json
 countries = constants.countries
 
 
-def business_news_feed():
+def business_news_feed(api_key):
     select_country = st.sidebar.selectbox("Select Country: ", countries.keys())
     st.header('NEWS FEED')
-    r = requests.get('https://newsapi.org/v2/top-headlines?country=' + countries[
-        select_country] + '&category=business&apikey=' + API_KEY)
+    r = requests.get('https://newsapi.org/v2/top-headlines?country=' + countries[select_country] + '&category=business&apikey=' + api_key)
     data_news = json.loads(r.content)
     length = min(15, len(data_news['articles']))
     for i in range(length):
@@ -37,6 +36,7 @@ def business_news_feed():
 
         url = data_news['articles'][i]['url']
         st.write(url)
+
 
 
 def isLeapYear(y):
@@ -174,7 +174,7 @@ START = "2016-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 year = int(TODAY[: 4])
 
-st.title('STOCKIFY: STOCK FORECAST APP')
+st.title('El SOUQ API: STOCK FORECAST APP')
 
 try:
     option = st.sidebar.selectbox("Which Dashboard?", ('Past Trends', 'Predict Stock Price', 'Trending Business News'),
