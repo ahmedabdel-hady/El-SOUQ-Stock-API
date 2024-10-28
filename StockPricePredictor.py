08ab6ab6490a99c356cf552988e6bf7e3e7367a2
Ahmed Abdelhady <ahmed.abdelhady@ejust.edu.eg>
2024-10-28 07:46:08 +0300

    Update StockPricePredictor.py

--- StockPricePredictor.py
+++ StockPricePredictor.py
@@ -1,22 +1,24 @@
 import streamlit as st
 from datetime import date
 from yahooquery import Ticker
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
+from prophet import Prophet
+from prophet.plot import plot_plotly
 import pandas as pd
 from plotly import graph_objs as go
-import config
+#import config
 import requests
 import constants
 import json
 
+API_KEY = st.secrets["NEWS_API_KEY"]
 countries = constants.countries
 
 
 def business_news_feed():
     select_country = st.sidebar.selectbox("Select Country: ", countries.keys())
     st.header('NEWS FEED')
-    r = requests.get('https://newsapi.org/v2/top-headlines?country=' + countries[select_country] + '&category=business&apikey=' + api_key)
+    r = requests.get('https://newsapi.org/v2/top-headlines?country=' + countries[
+        select_country] + '&category=business&apikey=' + API_KEY)
     data_news = json.loads(r.content)
     length = min(15, len(data_news['articles']))
     for i in range(length):
@@ -36,9 +38,6 @@ def business_news_feed():
 
         url = data_news['articles'][i]['url']
         st.write(url)
-
-# Replace 'api_key' with your actual API key
-api_key = 'f2cb4ed05496493589aabeb3bbb38699'
 
 
 def isLeapYear(y):
@@ -176,7 +175,7 @@ START = "2016-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 year = int(TODAY[: 4])
 
-st.title('ElSOUQ: STOCK FORECAST APP')
+st.title('STOCKIFY: STOCK FORECAST APP')
 
 try:
     option = st.sidebar.selectbox("Which Dashboard?", ('Past Trends', 'Predict Stock Price', 'Trending Business News'),
